bca55072a1242740ba819c57a72101500bb98295
Balázs Keresztúri <19balazs86@gmail.com>
2019-05-25 17:28:30 +0100

    Add RequestPreProcessor.

---
+++ PlayingWithMediatR/MediatR/Pipeline/RequestPreProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR.Pipeline;
+using Serilog;
+
+namespace PlayingWithMediatR.MediatR.Pipeline
+{
+  // This is loaded automatically when you AddMediatR.
+  public class RequestPreProcessor<TRequest> : IRequestPreProcessor<TRequest>
+  {
+    public Task Process(TRequest request, CancellationToken ct)
+    {
+      Log.Debug($"RequestPreProcessor: {typeof(TRequest).Name}.");
+
+      return Task.CompletedTask;
+    }
+  }
+}
