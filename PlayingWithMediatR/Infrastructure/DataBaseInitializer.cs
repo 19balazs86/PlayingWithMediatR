30c8dcf6e1ab2e2a00a15ed9ab480cbec0dfddf5
Balázs Keresztúri <19balazs86@gmail.com>
2019-06-19 20:44:14 +0100

    Remove the Bogus RuleFor(id).

--- PlayingWithMediatR/Infrastructure/DataBaseInitializer.cs
+++ PlayingWithMediatR/Infrastructure/DataBaseInitializer.cs
@@ -12,10 +12,8 @@ namespace PlayingWithMediatR.Infrastructure
 
       if (context.Products.Any()) return;
 
-      int id = 1;
-
       var products = new Faker<Product>()
-        .RuleFor(p => p.Id, _ => id++)
+        //.RuleFor(p => p.Id, _ => id++) // No need. The auto increment works well.
         .RuleFor(p => p.Name, f => f.Commerce.ProductName())
         .RuleFor(p => p.Price, f => f.Random.Number(10, 500))
         .RuleFor(p => p.Description, f => f.Lorem.Sentence())
