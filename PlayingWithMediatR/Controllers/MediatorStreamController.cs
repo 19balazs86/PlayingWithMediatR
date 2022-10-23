20690dfb2fad4ebbcc94dc89b911f9ca1b2467b9
Balazs Kereszturi <19balazs86@gmail.com>
2022-10-23 18:38:48 +0200

    Add example of streaming response with MediatR

---
+++ PlayingWithMediatR/Controllers/MediatorStreamController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PlayingWithMediatR.MediatR;
+
+namespace PlayingWithMediatR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediatorStreamController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MediatorStreamController(IMediator mediator) => _mediator = mediator;
+
+        [HttpGet("RandomNumbers/{amount}")]
+        public IAsyncEnumerable<int> GetRandomNumbers([FromRoute] int amount, CancellationToken cancellationToken)
+        {
+            var request = new GetRandomNumberRequest { AmountOfRandomNumbers = amount };
+
+            return _mediator.CreateStream(request, cancellationToken);
+        }
+    }
+}
---
+++ PlayingWithMediatR/MediatR/GetRandomNumberHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System.Runtime.CompilerServices;
+
+namespace PlayingWithMediatR.MediatR
+{
+    // https://youtu.be/2TT3suofNlo?t=511
+    public sealed class GetRandomNumberHandler : IStreamRequestHandler<GetRandomNumberRequest, int>
+    {
+        public async IAsyncEnumerable<int> Handle(GetRandomNumberRequest request, [EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            int counter = 0;
+
+            while (!cancellationToken.IsCancellationRequested && counter++ < request.AmountOfRandomNumbers)
+            {
+                await Task.Delay(1_000, cancellationToken);
+
+                yield return Random.Shared.Next();
+            }
+        }
+    }
+
+    public sealed class GetRandomNumberRequest : IStreamRequest<int>
+    {
+        public int AmountOfRandomNumbers { get; set; }
+    }
+}
